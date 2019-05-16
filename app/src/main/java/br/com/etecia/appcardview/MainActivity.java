03e0e651f2908112534aff4c782defa72e29d31e
manager <christopher.ferreira.silva@outlook.com>
2019-05-15 22:12:44 -0300

    Card view exercicio

---
+++ app/src/main/java/br/com/etecia/appcardview/MainActivity.java
@@ -0,0 +1,55 @@
+package br.com.etecia.appcardview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.idToolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setIcon(R.drawable.ic_arrow_back_dp);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_principal, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.mFavoritos: startActivity(new Intent(MainActivity.this, FavoritosActivity.class));
+            break;
+
+            case R.id.mSobre: startActivity(new Intent(MainActivity.this, SobreActivity.class));;
+            break;
+
+            case R.id.mConfiguracoes: startActivity(new Intent(MainActivity.this, ConfiguracoesActivity.class));;
+            break;
+
+            case R.id.mCompartilhar: startActivity(new Intent(MainActivity.this, CompartilharActivity.class));;
+            break;
+
+            case R.id.mTempo: startActivity(new Intent(MainActivity.this, TempoActivity.class));
+            break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
